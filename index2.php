9e835017ebb14b3cab76f934e6b524d5153652aa
Robert Moreira <robertmoreiracenteno@hotmail.com>
2014-08-05 23:29:12 -0500

    3 primeras paginas
    navegaciones iniciales

---
+++ index2.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Empezando con html</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ index3.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PAGINA3</title>
+</head>
+<body>
+
+</body>
+</html>
